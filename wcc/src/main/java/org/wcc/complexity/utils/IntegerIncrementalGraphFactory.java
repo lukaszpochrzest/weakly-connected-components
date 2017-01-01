180d8971fe5d2469d43e554532cbb78d4be15fcc
Łukasz Pochrzęst <luke.pochrzest@gmail.com>
2017-01-01 03:40:43 +0100

    Fix vertex factory

--- wcc/src/main/java/org/wcc/complexity/utils/IntegerIncrementalGraphFactory.java
+++ wcc/src/main/java/org/wcc/complexity/utils/IntegerIncrementalGraphFactory.java
@@ -6,6 +6,10 @@ public class IntegerIncrementalGraphFactory implements VertexFactory<Integer> {
 
     private static int i = 0;
 
+    public IntegerIncrementalGraphFactory() {
+        reset();
+    }
+
     @Override
     public Integer createVertex() {
         return i++;
